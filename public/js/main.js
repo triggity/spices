4a4b10c55cc5abd3f4ce1e18896a00b5f0bdb496
michael truong <truongmt@gmail.com>
2014-01-21 09:03:56 -0800

    basic backbone widget

--- public/js/main.js
+++ public/js/main.js
@@ -13,24 +13,25 @@ require.config({
     "backbone": {
       "deps": ["underscore", "jquery"],
       "exports": "Backbone"
-    },
-    "oauth": {
-      "exports": "oauth"
-    },
-    "tweenlite": {
-      "deps": ["cssplugin"],
-      "exports": "tweenlite"
-    },
-    "socketio": {
-      "exports": "io"
     }
   }
 });
 
 require([
-    'widgets/rate'
-    ], function(Rate) {
-  console.log('done');
-  var a = new Rate();
+  'backbone',
+  'widgets/rate'
+  ], function(Backbone, Rate) {
+  
+  $(document).ready(function() {
+    var b = new Backbone.Model({rate: 50});
+    var a = new Rate({model: b}, {title: 'something', period: 'yes'});
+    console.log(a);
+    $('#rates').append('<div></div>');
+    $('#rates').click(function() {
+      console.log('asdfsdf');
+    });
+    $('#rates').append(a.el);
+  
+  });
 
 });
--- public/js/widgets/rate.js
+++ public/js/widgets/rate.js
@@ -1,11 +1,24 @@
 define([
  'backbone'
  ], function(Backbone) {
-  var something = Backbone.View.extend({
-    initialize: function() {
-      console.log('no');           
+  var RateView = Backbone.View.extend({
+    className: 'rate',
+    initialize: function(attributes, options) {
+      var title = $('<div></div>')
+        .text(options.title)
+        .addClass('title');
+      var period = $('<div></div>')
+        .text(options.period)
+        .addClass('period');
+      var rate = $('<div></div>')
+        .text(this.model.get('rate'))
+      this.$el.append(title);
+      this.$el.append(rate);
+      this.$el.append(period);
     },
+    render: function() {
+    }
   
   });
-  return something;
+  return RateView;
 });
